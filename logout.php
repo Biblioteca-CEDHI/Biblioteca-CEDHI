558a4f333da2b88cc049e4090dfb2dbd0b9e60da
danielaperalesestrada <peralesdaniela82@gmail.com>
2025-09-21 16:26:37 -0500

    Cerrar sesión

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/app/auth.php';
+require_once __DIR__ . '/app/paths.php';
 
+session_start();
 logout();
-header("Location: index.php");
-exit;
+
+header("Location: " . url('index.php?logout=success'));
+exit();
+?>
